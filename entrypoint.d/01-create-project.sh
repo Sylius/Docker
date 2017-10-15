511d5583692b00d260a4ce08376457891a6d1802
Andreas de Pretis <ad@25th-floor.com>
2017-10-15 16:49:40 +0200

    Disable create-project on container startup - for now

--- entrypoint.d/01-create-project.sh
+++ entrypoint.d/01-create-project.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-cd ${BASE_DIR}
-#composer create-project sylius/sylius-standard app
-chmod +x app/bin/console
+#composer create-project sylius/sylius-standard ${SYLIUS_DIR} ${SYLIUS_VERSION}
+#chmod +x sylius/bin/console
